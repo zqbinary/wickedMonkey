55f8ce2b7e431ab9c413b21ea90f7e1ecf0402f4
zqbinary <zqbinary@foxmail.com>
2021-11-20 16:19:38 +0800

    <feat>f4 控制弹幕开关

--- biliMenu.user.js
+++ biliMenu.user.js
@@ -11,77 +11,77 @@
 (function () {
 
 
-	let listValue = ''
+    let listValue = ''
 
-	function getLessonList() {
-		const listSelect = '.list-box a'
-		const listsDom = document.querySelectorAll(listSelect);
-		let res = [];
-		for (let i = 0; i < listsDom.length; i++) {
-			const item = listsDom[i]
-			const lesson = {};
-			lesson.title = item.title;
-			lesson.p = item.querySelector('.page-num').innerText;
-			lesson.duration = item.querySelector('.duration').innerText;
-			lesson.url = item.href
-			if (!item.href.includes('http')) {
-				lesson.url = 'https://www.bilibili.com/' + lesson.href
-			}
-			res.push(lesson);
-		}
-		return res;
-	}
+    function getLessonList() {
+        const listSelect = '.list-box a'
+        const listsDom = document.querySelectorAll(listSelect);
+        let res = [];
+        for (let i = 0; i < listsDom.length; i++) {
+            const item = listsDom[i]
+            const lesson = {};
+            lesson.title = item.title;
+            lesson.p = item.querySelector('.page-num').innerText;
+            lesson.duration = item.querySelector('.duration').innerText;
+            lesson.url = item.href
+            if (!item.href.includes('http')) {
+                lesson.url = 'https://www.bilibili.com/' + lesson.href
+            }
+            res.push(lesson);
+        }
+        return res;
+    }
 
-	function makeLessonDoms(lists) {
-		let parent = document.createElement('div');
-		for (let item of lists) {
-			let itemDom = document.createElement('a')
-			itemDom.href = item.url
-			itemDom.innerText = `[${item.p}]${item.title}  (${item.duration})`
-			parent.appendChild(itemDom);
-			parent.appendChild(document.createElement('br'));
-			parent.appendChild(document.createElement('br'));
-		}
-		// document.body.appendChild(parent);
-		listValue = parent.innerHTML;
-	}
+    function makeLessonDoms(lists) {
+        let parent = document.createElement('div');
+        for (let item of lists) {
+            let itemDom = document.createElement('a')
+            itemDom.href = item.url
+            itemDom.innerText = `[${item.p}]${item.title}  (${item.duration})`
+            parent.appendChild(itemDom);
+            parent.appendChild(document.createElement('br'));
+            parent.appendChild(document.createElement('br'));
+        }
+        // document.body.appendChild(parent);
+        listValue = parent.innerHTML;
+    }
 
-	function cp() {
-		GM_setClipboard(listValue)
-	}
+    function cp() {
+        GM_setClipboard(listValue)
+    }
 
-	function cp2() {
-		GM_setClipboard(listValue, 'html')
-	}
+    function cp2() {
+        GM_setClipboard(listValue, 'html')
+    }
 
-	function handleBui() {
-		const check = document.querySelector('.bui-switch-input');
-		check.click();
-	}
-	function register() {
+    function handleBui() {
+        const check = document.querySelector('.bui-switch-input');
+        check.click();
+    }
+    function register() {
 
-		const btn = document.createElement('button')
-		btn.innerHTML = '复制/下载'
-		btn.addEventListener('click', () => {
-			const lists = getLessonList()
-			makeLessonDoms(lists)
-			cp()
-		})
-		btn.addEventListener('dblclick', () => {
-			const lists = getLessonList()
-			makeLessonDoms(lists)
-			cp2()
-		})
-		document.querySelector('.head-right').insertBefore(btn, document.querySelector('.next-button'))
-		
-		window.addEventListener('keydown',(e)=>{
-      if(e.code==='F4') {
-        handleBui();
-      }
-		},true)
-	}
-	onload = function () {
-		setTimeout(register, 1000)
+        const btn = document.createElement('button')
+        btn.innerHTML = 'c:t/dc:h'
+        btn.addEventListener('click', () => {
+            const lists = getLessonList()
+            makeLessonDoms(lists)
+            cp()
+        })
+        btn.addEventListener('dblclick', () => {
+            const lists = getLessonList()
+            makeLessonDoms(lists)
+            cp2()
+        })
+        document.querySelector('.head-right').insertBefore(btn, document.querySelector('.next-button'))
 
-	}
+        window.addEventListener('keydown', (e) => {
+            if (e.code === 'F4') {
+                handleBui();
+            }
+        }, true)
+    }
+    onload = function () {
+        setTimeout(register, 1000)
+
+    }
 })()
