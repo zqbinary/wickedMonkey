bb18cda0ac655fb7e4f89345dd27fc320e5c3373
西西里卷 <zqbinary@foxmail.com>
2022-08-15 09:18:11 +0800

    自动跳转 格式化

--- autoJumper.user.js
+++ autoJumper.user.js
@@ -1,24 +1,24 @@
 // ==UserScript==
-// @name         继续访问
+// @name         自动跳转
 // @version      1.0
-// @description  网站默认跳转,支持知乎，简书，掘金
-// @author       ningxixi
+// @description  网站默认跳转  支持 掘金，知乎
+// @author       宁西西
 // @match        https://link.zhihu.com/*
 // @match        https://www.jianshu.com/go-wild*
 // @match        https://link.juejin.cn/*
 // @grant        none
+// @icon         https://raw.githubusercontent.com/zqbinary/icons/53cb0c68ee5fbd05a1719e52b4e544ed5e9f5341/jumper.png
 // ==/UserScript==
 
 (function () {
     'use strict';
-    // Your code here...
-    setTimeout(function () {
+    setInterval(function () {
         redirect();
-    }, 1000);
+    }, 300);
 
     const host = location.host
-    console.log('hhh',host)
-    function redirect(){
+
+    function redirect() {
         switch (host) {
             case 'link.zhihu.com':
                 jump("a.button", "继续访问")
@@ -29,16 +29,17 @@
             case 'link.juejin.cn':
                 jump("button.btn")
                 break
+
         }
     }
 
     function jump(selector, text = '继续') {
         const dom = document.querySelector(selector)
-        if (dom && dom.innerText.includes(text)) {
+        if (dom && dom.innerText && dom.innerText.includes(text)) {
             dom.click()
         }
     }
-    /**
+    /**example
 	 * @test1
 	 * https://www.jianshu.com/go-wild?ac=2&url=https%3A%2F%2Fnodejs.org%2Fdist%2Flatest-v6.x%2Fdocs%2Fapi%2Fhttp.html%23http_class_http_incomingmessage
 	 * @test2
