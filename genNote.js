4a756f9cbf82f0946ef5e2e8323a266e52515622
西西里卷 <zqbinary@foxmail.com>
2023-02-28 19:58:10 +0800

    br 逗号

--- genNote.js
+++ genNote.js
@@ -9,33 +9,31 @@ const fileFullPath = arguments[0];
 let ORIGIN_INFO;
 try {
     ORIGIN_INFO = path.parse(fileFullPath)
-    console.log(ORIGIN_INFO)
 } catch (e) {
     console.log("解析错误", e);
     process.exit(10)
 }
-const BR = `<div><br></div> `
+//todo 如果是文件夹，下面的html,排除 _n_
+const BR = `<div><br></div>`
 const BLOCK_START = `<table style=border-collapse:collapse;min-width:100%><col style=width:926px><col style=width:413px>`;
 const TR_START = `<tr><td style="width:926px;padding:8px;border:1px solid">`
 const TR_END = `</td><td style="width: 413px; padding: 8px; border: 1px solid;"><div><br /></div></td>`
 const BLOCK_END = `</tr></tbody></table>`
-// const TARGET_DIR = path.join(ORIGIN_INFO['dir'], 'out')
 const TARGET_DIR = path.join(ORIGIN_INFO['dir'], '')
 if (!fs.existsSync(TARGET_DIR)) {
-    fs.mkdirSync(TARGET_DIR)
+    fs.mkdirSync(TARGET_DIR, true)
 }
+
 function getFormatBody() {
     let content = fs.readFileSync(fileFullPath, 'utf-8')
     const bodyReq = /<body[^>]*>(.*)<\/body>/s
     //取body
     content = (bodyReq.exec(content))[1]
     //替换带格式的代码
-    content = content.replaceAll('<pre class=" CodeMirror-line " role="presentation">', '<div>')
+    content = content.replace(/<pre.*?>/igs, '<div>')
     content = content.replaceAll('</pre>', '</div>')
-    // content = content.replaceAll('src="assets/', `src="../assets/`)
     //src="assets/ 处理图片
     return content;
-
 }
 
 function getH1Block(content) {
@@ -47,46 +45,46 @@ function getH1Block(content) {
     }
     let block
     for (let i = seperators.length - 1; i >= 0; i--) {
-        let seperator = seperators[i];
-        [content, block] = content.split(seperator)
-        blocks.unshift(seperator + block);
+        let separator = seperators[i];
+        [content, block] = content.split(separator)
+        blocks.unshift(separator + block);
     }
     return blocks;
 }
 
 function handleH1Block(content, title) {
     const HTML_START = `<html><title>${title}</title><meta content="text/html;charset=utf-8"http-equiv=Content-Type><meta content="Evernote Windows/308626 (zh-CN, DDL); Windows/10.0.0 (Win64);"name=exporter-version>
-<style>*{font-family:微软雅黑;font-size:11pt}h1{color:red;}h2{color:orangered;}img{max-width:100%;}</style>`
+<style>*{font-family:微软雅黑;font-size:11pt}h1{color:red;}h2{color:rebeccapurple;}img{max-width:100%;}</style>`
     const HTML_END = `</body></html>`
     let output = '';
     const h2Req = /<h2(.+?)<\/h2>/g
 
-    let seperators = content.match(h2Req) || []
-    if (!seperators.length || seperators.length === 1) {
+    let separators = content.match(h2Req) || []
+    if (!separators.length || separators.length === 1) {
         output += BLOCK_START + TR_START + content + TR_END + BLOCK_END;
     }
     let arr = []
-
-    for (let i = seperators.length - 1; i >= 0; i--) {
-        let seperator = seperators[i];
-        let blockContent = ''
-        let block
-        [content, block] = content.split(seperator)
-        blockContent += BLOCK_START + TR_START
+    let block = ''
+    let blockContent = ''
+    for (let i = separators.length - 1; i >= 0; i--) {
+        let separator = separators[i];
+        [content, block] = content.split(separator)
+        separator += '<div>正文:</div>'
+        blockContent = BLOCK_START + TR_START
         if (i === 0) {
             blockContent += content + TR_END
                 + BLOCK_END + BR + BLOCK_START
-                + TR_START + seperator + TR_END
+                + TR_START + separator + TR_END
                 + handleH3(block)
                 + TR_START + TR_END;
         } else {
-            blockContent += seperator + TR_END
+            blockContent += separator + TR_END
                 + handleH3(block)
         }
         blockContent += TR_START + TR_END + BLOCK_END + BR
         arr.unshift(blockContent)
     }
-    output += arr.join()
+    output += arr.join(BR)
     output += BR
     let i = 6
     while (i-- > 0) {
@@ -102,14 +100,14 @@ function handleH1Block(content, title) {
 //对block,h3 进行处理
 function handleH3(h3Content) {
     const blockReq = /<h3(.+?)<\/h3>/g
-    let seperators = h3Content.match(blockReq) || []
-    if (!seperators.length || seperators.length === 1) {
+    let separators = h3Content.match(blockReq) || []
+    if (!separators.length || separators.length === 1) {
         return TR_START + h3Content + TR_END
     }
     let res = []
-    for (let i = seperators.length - 1; i >= 0; i--) {
+    for (let i = separators.length - 1; i >= 0; i--) {
         let bl;
-        let sep = seperators[i];
+        let sep = separators[i];
         [h3Content, bl] = h3Content.split(sep)
         res.unshift(TR_START + sep + BR + bl + TR_END)
     }
@@ -117,7 +115,6 @@ function handleH3(h3Content) {
 }
 
 function getH1Title(h1Block) {
-    //<h1 id='1认识微服务'><span>1.认识微服务</span></h1><p>
     const h1NameReq = /<h1\s+id=["|'](.*?)["|']>/i
     let res = h1Block.match(h1NameReq)
     if (res == null || res.length < 2) {
---
+++ genNote.md
@@ -0,0 +1,7 @@
+# 调用方法
+
+## win
+1. 用vscode 打开目录
+2. ctrl+shift+` 打开终端，输入命令
+@Administrator ➜ 讲义 node D:\tools\wickedMonkey\genNote.js .\SpringCloud01.html
+3 解析后，open with live server
